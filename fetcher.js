bd80e37224012108832a9d644f844539cd58992e
Hamanpreet <haman42626@gmail.com>
2023-05-18 19:44:15 -0400

    Return invalid for wrong URL(edge case)

--- fetcher.js
+++ fetcher.js
@@ -1,3 +1,4 @@
+// import the request, fs and readline libraries
 const request = require('request');
 const fs = require('fs');
 const readline = require('readline');
@@ -13,21 +14,23 @@ request(arr[0], (error, response, body) => {
   if(error) {
     return console.log('URL is invalid'); // Print the error if one occurred
   }
-console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  // Print the response status code if a response was received
+  console.log('statusCode:', response && response.statusCode);
+  // Print the HTML for the Google homepage.
+  console.log('body:', body);
   fs.writeFile(arr[1], body, err => {
     if(err) {
       console.log("it is invalid");
     }
   });
+
+  //calculating size in bytes this way
   fs.readFile(arr[1], 'utf8', (err, data) => {
      if (err) {
     console.error('Error reading file:', err);
     return;
   }
-
   const byte = data.length;
   console.log(`Downloaded and saved ${byte} bytes to ${arr[1]}`);
-  
   });
 });
